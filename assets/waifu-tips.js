7a42d2b7bbe186e6c7a60ccdc7582f80e47ea587
biguokang <biguokang@outlook.com>
2018-12-04 17:17:29 +0800

    在后端设置了下素材服务器的对外服务，现在可以直接使用了

--- assets/waifu-tips.js
+++ assets/waifu-tips.js
@@ -357,7 +357,7 @@ function loadModel(modelId, modelTexturesId){
     localStorage.setItem('modelId', modelId);
     if (modelTexturesId === undefined) modelTexturesId = 0;
     localStorage.setItem('modelTexturesId', modelTexturesId);
-    loadlive2d('live2d', 'http://live2d.biguokang.cn/live2d/get/index.php?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
+    loadlive2d('live2d', 'http://api.biguokang.cn/get/index.php?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
 }
 
 function loadRandModel(){
@@ -368,7 +368,7 @@ function loadRandModel(){
     
     $.ajax({
         cache: false,
-        url: 'http://live2d.biguokang.cn/live2d/'+modelTexturesRandMode+'_textures/index.php?id='+modelId+'-'+modelTexturesId,
+        url: 'http://api.biguokang.cn/'+modelTexturesRandMode+'_textures/index.php?id='+modelId+'-'+modelTexturesId,
         dataType: "json",
         success: function (result){
             if (result.textures['id'] == 1 && (modelTexturesId == 1 || modelTexturesId == 0)) {
@@ -388,7 +388,7 @@ function loadOtherModel(){
     
     $.ajax({
         cache: false,
-        url: 'http://live2d.biguokang.cn/live2d/'+modelTexturesRandMode+'/index.php?id='+modelId,
+        url: 'http://api.biguokang.cn/'+modelTexturesRandMode+'/index.php?id='+modelId,
         dataType: "json",
         success: function (result){
             loadModel(result.model['id']);
